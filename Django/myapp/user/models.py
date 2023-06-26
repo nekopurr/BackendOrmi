97c4f1f8d3e0c43edb40817214c192153b8c55ce
이병훈 <85627591+nekopurr@users.noreply.github.com>
2023-06-26 21:29:35 +0900

    Django

--- Django/myapp/user/models.py
+++ Django/myapp/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=255) # unique=True : PK지정
+    name = models.CharField(max_length=50, null=True, blank=True) # 비어도 무관하게(null : 0허용, blank : 비어도 ㄱㅊ 보통 두개 같이 씀)
+    password = models.CharField(max_length=50)
+    registered_date = models.DateTimeField(auto_now_add=True) # 최초 생성 시 한번
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.name
